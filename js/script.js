d2e332ec6c9335e2c4917e30df447af38eceffae
f-4se <159545697+f-4se@users.noreply.github.com>
2025-03-11 21:43:00 +0545

    Commit

--- js/script.js
+++ js/script.js
@@ -8,3 +8,25 @@ document.getElementById("moreBtn").addEventListener("click", function () {
         this.textContent = "🔽";
     }
 });
+
+let slideIndex = 0;
+showSlides(slideIndex);
+
+function changeSlide(n) {
+    showSlides(slideIndex += n);
+}
+
+function showSlides(n) {
+    let slides = document.getElementsByClassName("slide");
+
+    if (n >= slides.length) slideIndex = 0;
+    if (n < 0) slideIndex = slides.length - 1;
+
+    for (let i = 0; i < slides.length; i++) {
+        slides[i].classList.remove("active");
+    }
+
+    slides[slideIndex].classList.add("active");
+}
+
+setInterval(() => changeSlide(1), 3000);
